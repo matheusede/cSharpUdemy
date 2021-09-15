f447b3b5f03e6322d0718a93ad34d116f0b28fec
Matheus Marcolino <81192295+matheusede@users.noreply.github.com>
2021-09-15 02:31:49 -0300

    ex02
    Exercício que tem como intuito fixar os tipos de inputs de dados na linguagem c#

---
+++ Ex02/Ex02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex02 {
+    class Program {
+        static void Main(string[] args) {
+            //Declaração de variáveis
+            string nomeCompleto, ultimoNome;
+            int quartos, idade;
+            float preco, altura;
+
+            //Leitura de dados
+            Console.WriteLine("Entre com seu nome completo:");
+            nomeCompleto = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            preco = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            string[] array = Console.ReadLine().Split(' ');
+            ultimoNome = array[0];
+            idade = int.Parse(array[1]);
+            altura = float.Parse(array[2], CultureInfo.InvariantCulture);
+
+            //Output de dados
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
