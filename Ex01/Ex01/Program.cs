cc340ef4f08d6d9e9891b373838099ae92556be3
Matheus Marcolino <81192295+matheusede@users.noreply.github.com>
2021-09-14 23:33:24 -0300

    Ex01
    Primeiro exercício do curso de C# disponível na udemy. O código aborda os diferentes tipos de output de dados presentes na linguagem.

---
+++ Ex01/Ex01/Program.cs
@@ -0,0 +1,27 @@
+﻿//Exercício que tem como intuito demonstrar os tipos de outputs de dados em c#
+using System;
+using System.Globalization;
+
+namespace Ex01 {
+    class Program {
+        static void Main(string[] args) {
+
+            //Declaração de variáveis
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+            double preco1 = 2100.0, preco2 = 650.50, medida = 53.234567;
+
+            //Output dos dados usando as diversas opções de formatações e outputs do c#
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preco é ${preco1}");
+            Console.WriteLine($"{produto2}, cujo preco é ${preco2}\n");
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}\n");
+            Console.WriteLine($"Medida com oito casas decimais: {medida}");
+            Console.WriteLine($"Arredondando (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
