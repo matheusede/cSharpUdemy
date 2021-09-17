2b7b2953df0a429395b3e77ddcd2f12a46f02301
Matheus Marcolino <81192295+matheusede@users.noreply.github.com>
2021-09-17 20:05:35 -0300

    ExFixacao04

---
+++ Unidade 04/ExFixacao03/ExFixacao03/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExFixacao03 {
+    class Aluno {
+        public string Nome;
+        public float Nota1, Nota2, Nota3;
+
+        public float CalculaNotaFinal() {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool Aprovado() {
+            if (CalculaNotaFinal() >= 60.0f)
+                return true;
+            else
+                return false;
+
+        }
+
+        public float VerificaNota() {
+            if (CalculaNotaFinal() >= 60f)
+                return 0.0f;
+            else
+                return 60.0f - CalculaNotaFinal();
+        }
+    }
+}
---
+++ Unidade 04/ExFixacao03/ExFixacao03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExFixacao03 {
+    class Program {
+        static void Main(string[] args) {
+            Aluno aluno = new Aluno();
+            Console.WriteLine("Digite o nome do aluno.");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno.");
+            aluno.Nota1 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"NOTA FINAL = {aluno.CalculaNotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+            if (aluno.Aprovado())
+                Console.WriteLine("APROVADO");
+            else
+                Console.WriteLine($"REPROVADO \nFALTARAM {aluno.VerificaNota().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
